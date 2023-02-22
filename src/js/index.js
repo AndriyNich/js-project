3a13c993b146f42552f08857d392f9cc8840ef97
AndriyNich <info@bembi.ua>
2023-02-23 00:11:18 +0200

    5

---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+// query films https://api.themoviedb.org/3/discover/movie?api_key=2ea7c46495834bb72f73e9f64cf920c5&language=uk-uk
+// load image - https://image.tmdb.org/t/p/w300_and_h450_bestv2/fhMxcURSaKFIKoMib4JewuxfGOM.jpg
+// look for ID= 245891 https://api.themoviedb.org/3/movie/245891?api_key=2ea7c46495834bb72f73e9f64cf920c5&language=uk-uk
+// look for query string "wick"  - https://api.themoviedb.org/3/search/movie?api_key=2ea7c46495834bb72f73e9f64cf920c5&query='Wick'
+// look for collection https://api.themoviedb.org/3/collection/{collection_id}?api_key=<<api_key>>&language=en-US
